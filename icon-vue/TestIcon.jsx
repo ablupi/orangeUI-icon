7471fafa1b9c05d127bf8c5669888276579f29e9
ou <957397192@qq.com>
2024-08-29 14:10:26 +0800

    🔥 优化项目结构

--- icon-vue/TestIcon.jsx
+++
@@ -1,8 +0,0 @@
-import Icon from '../src/components/Icon.jsx';
-
-const AccountBookFilled = (props, context) => {
-  // const p = { ...props, ...context.attrs };
-  return <Icon ></Icon>;
-};
-
-export default AccountBookFilled;
--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-
-import {setupCounter} from './counter'
-
 document.querySelector('#app').innerHTML = `
   <div>
     主程序页面
@@ -9,3 +6,12 @@ document.querySelector('#app').innerHTML = `
 
 setupCounter(document.querySelector('#counter'))
 
+function setupCounter(element) {
+  let counter = 0
+  const setCounter = (count) => {
+    counter = count
+    element.innerHTML = `count is ${counter}`
+  }
+  element.addEventListener('click', () => setCounter(counter + 1))
+  setCounter(0)
+}
